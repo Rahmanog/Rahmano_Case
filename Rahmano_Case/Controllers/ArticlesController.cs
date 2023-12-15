cecba7eca2257900fe3f06242f67dbce65d59fe9
sitahman0 <44361250+sitahman0@users.noreply.github.com>
2023-12-15 20:46:55 +0700

    Satu

---
+++ Rahmano_Case/Controllers/ArticlesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rahmano_Case.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rahmano_Case/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rahmano_Case.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
