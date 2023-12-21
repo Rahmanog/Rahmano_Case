af76e0318d89188a6dc534baacb246fa2316d848
sitahman0 <44361250+sitahman0@users.noreply.github.com>
2023-12-21 08:05:08 +0700

    20 Des

--- Rahmano_Case/Controllers/HomeController.cs
+++ Rahmano_Case/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Rahmano_Case.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My - Profile ";
 
             return View();
         }
